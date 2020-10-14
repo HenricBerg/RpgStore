495af3d72996b3f7d6fd7e0500777aa6c4407c64
Henric Berg <henric_berg@live.se>
2020-10-14 22:12:48 +0200

    Add project files.

---
+++ RpgStore/Server/Controllers/TextController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RpgStore.Shared;
+
+namespace RpgStore.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TextController : ControllerBase
+    {
+        [HttpGet]
+        public Text Get()
+        {
+            return new Text
+            {
+                Name = "TestName",
+                Description = "TestDescription",
+                TextContent = "TestContent",
+                Modified = DateTime.UtcNow
+            };
+        }
+
+        [HttpPost]
+        public void Post(Text text)
+        {
+            var text1 = text;
+        }
+    }
+}
---
+++ RpgStore/Shared/Text.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RpgStore.Shared
+{
+    public class Text
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string TextContent { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
